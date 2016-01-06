cbe7442ead6ff1f704efd423c87c96c7a0709a9a
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2016-01-06 22:47:22 +1030

    Use .env file for configuration

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
+require('dotenv').load()
+
 window.__CLIENT__ = true
 window.__SERVER__ = false
 window.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production'
-window.__API_ROOT__ = __DEVELOPMENT__ ? 'http://www.pjgalbraith.local/wp-json/wp/v2/' : 'https://www.pjgalbraith.com/wp-json/wp/v2/'
+window.__API_ROOT__ = process.env.API_ROOT
 
 require('./client')
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
 require("babel-register")
+require('dotenv').load()
 
 global.__CLIENT__ = false
 global.__SERVER__ = true
-global.__DISABLE_SSR__ = false  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
+global.__DISABLE_SSR__ = process.env.DISABLE_SSR  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production'
-global.__API_ROOT__ = __DEVELOPMENT__ ? 'http://www.pjgalbraith.local/wp-json/wp/v2/' : 'https://www.pjgalbraith.com/wp-json/wp/v2/'
+global.__API_ROOT__ = process.env.API_ROOT
 
 require('./server')
